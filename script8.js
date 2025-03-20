5b25dee9d34c6930ad789fd4722dfbdaf701996a
ananthakrishnanpta <ananthakrishnanpta@gmail.com>
2025-03-20 11:06:34 +0530

    edits

--- script8.js
+++ script8.js
@@ -29,6 +29,27 @@
         for (cell of tds){
             cell.style.backgroundColor = "lightblue";
         }
-        
-        
     }
+ 
+// key events
+const ball = document.getElementById('ball'); // reference to ball
+
+//  initializing variables to track the position of the ball
+let x = 0, y = 0;
+
+function game(event){
+    if (event.key === 'ArrowUp') y -= 10;
+    if (event.key === 'ArrowDown') y += 10;
+    if (event.key === 'ArrowLeft') x -= 10;
+    if (event.key === 'ArrowRight') x += 10;
+
+    // Set new position
+    ball.style.transform = `translate(${x}px, ${y}px)`;
+    ball.classList.toggle('dark');
+    ball.classList.toggle('light');
+}
+
+document.addEventListener('keydown', game);
+
+
+
